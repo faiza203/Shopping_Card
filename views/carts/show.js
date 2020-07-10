b18deb7e80cc719bdb3a797f20308125654c4e51
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-10 11:16:41 +0500

    insert styles in carts

--- views/carts/show.js
+++ views/carts/show.js
@@ -4,15 +4,51 @@ module.exports = ({ items }) => {
   const renderedItems = items
     .map(item => {
       return `
-      <div>${item.product.title} - ${item.product.price}</div>
-    `;
+        <div class="cart-item message">
+          <h3 class="subtitle">${item.product.title}</h3>
+          <div class="cart-right">
+            <div>
+              $${item.product.price}  X  ${item.quantity} = 
+            </div>
+            <div class="price is-size-4">
+              $${item.product.price * item.quantity}
+            </div>
+            <div class="remove">
+              <form method="POST">
+                <button class="button is-danger">                  
+                  <span class="icon is-small">
+                    <i class="fas fa-times"></i>
+                  </span>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      `;
     })
     .join('');
 
   return layout({
     content: `
-      <h1>Cart</h1>
-      ${renderedItems}
+      <div id="cart" class="container">
+        <div class="columns">
+          <div class="column"></div>
+          <div class="column is-four-fifths">
+            <h3 class="subtitle"><b>Shopping Cart</b></h3>
+            <div>
+              ${renderedItems}
+            </div>
+            <div class="total message is-info">
+              <div class="message-header">
+                Total
+              </div>
+              <h1 class="title">$</h1>
+              <button class="button is-primary">Buy</button>
+            </div>
+          </div>
+          <div class="column"></div>
+        </div>
+      </div>
     `
   });
 };
