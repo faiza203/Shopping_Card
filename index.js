ab812970e106712529681290b220bac5820013bf
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-10 11:16:13 +0500

    insert images

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
 const app = express();
-app.get("/",(req,res)=>{
-    res.send( `
-    <div>
-    <form method="POST">
-    <h1>Sing Up</h1>
-    <input name="name" type="text" placeholder="Name" required />
-   <input name="email" type="email" placeholder="Email" required /> 
-   <input name="password" type="password" placeholder="Password" required />
-   <input name="confirm_password" type="password" placeholder="Confirm Password" required /> 
-   <button>Sign Up</button>
-    </form>
-    </div>`
-    )
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
+);
+app.use(authRouter);
+app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
+
+app.listen(3000, () => {
+  console.log('Listening');
 });
-app.post("/",(req,res)=>{
-    res.send("Successfully Created!!!")
-})
-app.listen(3000,()=>{
-    console.log("listenging");
-})
